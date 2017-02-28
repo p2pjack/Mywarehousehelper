4f184eb3d73786a8437f1505aa9a3019dfa94b74
Eaun-Ballinger <p2pjack@gmail.com>
2017-02-28 21:53:53 +0000

    fixed it lin and grid selectable : )

--- mobile/src/main/java/com/uk/umf_solutions/warehousehelper/ui/whereMyStockFunction/WhereMyStockFragment_UI.java
+++ mobile/src/main/java/com/uk/umf_solutions/warehousehelper/ui/whereMyStockFunction/WhereMyStockFragment_UI.java
@@ -3,6 +3,7 @@ package com.uk.umf_solutions.warehousehelper.ui.whereMyStockFunction;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -20,10 +21,21 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class WhereMyStockFragment_UI extends Fragment implements WmsOnClickListener {
-    private View mRootView;
-    private WmsListAdapter mAdapter;
-    private RecyclerView mRecyclerView;
-    private TextView mEmptyText;
+
+    private static final String TAG = "WhereMyStockFragment_UI";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+
+    protected LayoutManagerType mCurrentLayoutManagerType;
+    protected RecyclerView mRecyclerView;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected WmsListAdapter mAdapter;
+    protected TextView mEmptyText;
 
     public WhereMyStockFragment_UI() {
         // Required empty public constructor
@@ -31,40 +43,73 @@ public class WhereMyStockFragment_UI extends Fragment implements WmsOnClickListe
 
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mRecyclerView = (RecyclerView) container.findViewById(R.id.wms_recycler_view);
-        mEmptyText = (TextView) container.findViewById(R.id.empty_text);
-        // Inflate the layout for this fragment
-        mRootView = inflater.inflate(R.layout.fragment_where_my_stock_fragment__ui, container, false);
+        View mRootView = inflater.inflate(R.layout.fragment_where_my_stock_fragment__ui, container, false);
+        mRootView.setTag(TAG);
+        mRecyclerView = (RecyclerView) mRootView.findViewById(R.id.wms_recycler_view);
+        // LinearLayoutManager is used here, this will layout the elements in a similar fashion
+        // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
+        // elements are laid out.
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
 
-        //setup RecyclerView
-        ArrayList<whereMyStock> tempWms = new ArrayList<>();
-
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-            mAdapter = new WmsListAdapter(tempWms,getContext(),this);
-            mRecyclerView.setLayoutManager(layoutManager);
-            mRecyclerView.setAdapter(mAdapter);
-
-
-        if (tempWms.size() < 1){
-            showEmptyTextMessage();
-        }else{
-            hideEmptyTextMessage();
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
         }
-
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+        ArrayList<whereMyStock> tempWms = new ArrayList<>();
+        mAdapter = new WmsListAdapter(tempWms,getContext(),this);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        setRecyclerViewLayoutManager(LayoutManagerType.LINEAR_LAYOUT_MANAGER);
         return mRootView;
     }
+    /**
+     * Set RecyclerView's LayoutManager to the one given.
+     *
+     * @param layoutManagerType Type of layout manager to switch to.
+     */
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
 
-    private void hideEmptyTextMessage() {
-        mEmptyText.setVisibility(View.GONE);
-        mRecyclerView.setVisibility(View.VISIBLE);
+        // If a layout manager has already been set, get current scroll position.
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        switch (layoutManagerType) {
+            case GRID_LAYOUT_MANAGER:
+                mLayoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
+                mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+                break;
+            case LINEAR_LAYOUT_MANAGER:
+                mLayoutManager = new LinearLayoutManager(getActivity());
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+                break;
+            default:
+                mLayoutManager = new LinearLayoutManager(getActivity());
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        }
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
     }
 
-    private void showEmptyTextMessage() {
-
-        mRecyclerView.setVisibility(View.GONE);
-        mEmptyText.setVisibility(View.VISIBLE);
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save currently selected layout manager.
+        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
+        super.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
