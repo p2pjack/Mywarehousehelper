4d3989768e265dde7f5d722df40266c037354972
Eaun-Ballinger <p2pjack@gmail.com>
2017-03-07 12:47:24 +0000

    Realm branch created

--- mobile/src/main/java/com/uk/umf_solutions/warehousehelper/Data/DatabaseHelper.java
+++ mobile/src/main/java/com/uk/umf_solutions/warehousehelper/Data/DatabaseHelper.java
@@ -6,4 +6,54 @@ package com.uk.umf_solutions.warehousehelper.Data;
  */
 
 public class DatabaseHelper {
+
+//
+//    public DatabaseHelper(Context context) {
+//    }
+//
+//    public boolean createUser(User user) {
+//        try {
+//            realm.beginTransaction();
+//            realm.copyToRealm(user);
+//            realm.commitTransaction();
+//            realm.close();
+//            return true;
+//        } catch (RealmException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+//
+//    public boolean deleteUser(String email) {
+//        try {
+//            RealmResults<User> user = realm.where(User.class).equalTo(DbConstants.EMAIL_KEY, email)
+//                    .findAll();
+//            user.clear();
+//            return true;
+//        } catch (RealmException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+//
+//    public User readUser(String email) {
+//        User user = realm.where(User.class).equalTo(DbConstants.EMAIL_KEY, email)
+//                .findFirst();
+//        return user;
+//    }
+//
+//    public boolean isUserExist(String email) {
+//        try {
+//            RealmResults<User> user = realm.where(User.class).equalTo(DbConstants.EMAIL_KEY, email)
+//                    .findAll();
+//            if (user.size() > 0) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        } catch (RealmException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
 }
---
+++ mobile/src/main/java/com/uk/umf_solutions/warehousehelper/constants/constants.java
@@ -0,0 +1,13 @@
+package com.uk.umf_solutions.warehousehelper.constants;
+
+/**
+ * Created by Eaun-Ballinger on 06/03/2017.
+ *
+ */
+
+public class constants {
+
+    public static final String wmsId = "id";
+    public static final String wmsAction = "action";
+    public static final String wmsMessage = "message";
+}
